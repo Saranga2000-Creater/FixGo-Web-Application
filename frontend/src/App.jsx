b22833ce088768ea67886292ae17cd24004059a4
Saranga2000-Creater <sarangapieris809@gmail.com>
2026-05-28 14:59:18 +0530

    Initail project start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-4xl text-white bg-blue-600 flex justify-center p-3 font-mono  ' >
+        <h1>Welcome to the Frontend!</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
